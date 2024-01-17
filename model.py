b60f0188339c66dece1c2fe5cd9642fd18a21147
mhtruong1031 <mhtruong1031@gmail.com>
2024-01-17 00:29:08 -0800

    added secondary univariate

--- model.py
+++ model.py
@@ -1,51 +1,51 @@
-from .variables import *
+from pgmpy.models import BayesianNetwork
+from variables import *
 
 '''
 Using the Integrated Motivational-Volitional Model (IVM) as a Framework
 
-Diathesis:
-Q78 - Screen time 
-Q86 - Sleep
-Q87 - Homelessness 
-Q94 - Job loss during pandemic 
+Diathesis
+QN87: Usually did not sleep in their parent's or guardian's home
+QN94: Reported that an adult in their home lost their job during the COVID-19 pandemic 	
 
-Defeat:
-Q23 - Bullying at school 
-Q24 - Electronic bullying
-Q19 - Forced sexual intercourse 
-Q20 - Sexual violence 
-Q22 - Physical dating violence
+Defeat
+Q23_new: Bullied
+    Q23: Bullying at school 
+    Q24: Electronic bullying
+QN22_new: Sexual or Dating Violence
+    Q19: Forced sexual intercourse 
+    QN22: Physical dating violence
 
-Threat to Self Moderators:
-Q66 - Perception of weight 
-Q77 - Physical activity >= 5 days 
-Q98 - Difficulty concentrating
-QNILLICT - Ever used select illicit drugs
+Threat to Self Moderators
+QN66: Negative Perception of weight 
+QN67_new: Negative Perception of and Trying to Lose Weight
+    QN67: Trying to lose weight
+Q98: Difficulty concentrating
+QNILLICT: Ever used select illicit drugs
 
-Entrapment:
-Q15 - Threatened at school
-Q85 - Current mental health
-QN85 - Reported that their mental health was most of the time or always not good 
-Q93 - Mental health during pandemic
-QN93 - Reported that their mental health was most of the time or always not good during the COVID-19 pandemic 
+Entrapment
+QN85: Reported that their mental health was most of the time or always not good 
+QN93: Reported that their mental health was most of the time or always not good during the COVID-19 pandemic 
 
-Motivational Moderations:
-Q25 - Sad or hopeless 
-Q96 - Feel close to people at their school
+Motivational Moderations
+Q25: Sad or hopeless 
+QN96: Feel close to people at their school
 
-Ideation:
-Q26 - Considered suicide 
+Ideation
+Q26: Considered suicide 
 
-Volitional Moderators:
-Q13 - Gun carrying
-Q16 - Physical fighting
-Q18 - Saw physical violence in neighborhood
-Q27  - Made a suicide plan
+Volitional Moderators
+QN13_new: Carried a weapon
+    QN12: Carried a weapon on school property
+    QN13: Carried a gun
+QN16: Were in a physical fight 
+Q18: Saw physical violence in the neighborhood
+Q27: Made a suicide plan
 
-
-Attempts:
-Q28 - Attempted suicide
+Behavior
+QN28: Attempted suicide
 '''
+
 
 # // DIATHESIS //
 
--- univariate.py
+++ univariate.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from scipy.stats import chi2_contingency
 from variables import relationships
 
 regression = LogisticRegression()
 
-def get_univariate_results(input: pd.Series, output: pd.Series):
+def get_uni_logit_results(input: pd.Series, output: pd.Series):
     clean_data = pd.concat([input, output], axis=1).dropna() # remove Nan values
     n_samples  = len(clean_data.index)
 
@@ -15,18 +16,28 @@ def get_univariate_results(input: pd.Series, output: pd.Series):
 
     return regression.score(x, y)
 
+def get_uni_chi2_results(input: pd.Series, output: pd.Series):
+    clean_data        = pd.concat([input, output], axis=1).dropna() # remove Nan values
+    contingency_table = pd.crosstab(clean_data[output.name], clean_data[input.name])
+
+    return chi2_contingency(contingency_table).pvalue
+
 def main() -> None:
-    log = "Univariate Results\n--------------------------------------------------------------------\n"
+    log_logit = "Univariate Results\n--------------------------------------------------------------------\n"
+    log_chi2  = "Univariate Results\n--------------------------------------------------------------------\n"
     for relationship in relationships:
         input  = relationship[0]
         output = relationship[1]
 
         for var_i in input:
             for var_o in output:
-                log += f"i: {var_i.name}  o: {var_o.name}  |  coef: {get_univariate_results(var_i, var_o)}\n"
-    print(log)
-    with open('results/univariate.txt', 'a') as f:
-        f.write(log)
+                log_logit += f"i: {var_i.name}  o: {var_o.name}  |  coef: {get_uni_logit_results(var_i, var_o)}\n"
+                log_chi2  += f"i: {var_i.name}  o: {var_o.name}  |  coef: {get_uni_chi2_results(var_i, var_o)}\n"
+    
+    with open('results/uni_logit.txt', 'w') as f:
+        f.write(log_logit)
+    with open('results/uni_chi2.txt', 'w') as f:
+        f.write(log_chi2)
 
 
 if __name__ == '__main__':
