1904628b41b3059c4c98e551505af8866b222545
mhtruong1031 <mhtruong1031@gmail.com>
2024-01-18 15:54:10 -0800

    chi square univariate

--- univariate.py
+++ univariate.py
@@ -5,17 +5,18 @@ from variables import relationships
 
 regression = LogisticRegression()
 
+# returns r-value of logistic regression
 def get_uni_logit_results(input: pd.Series, output: pd.Series):
     clean_data = pd.concat([input, output], axis=1).dropna() # remove Nan values
-    n_samples  = len(clean_data.index)
 
     x = clean_data[input.name].to_numpy().reshape((-1, 1))
-    y = clean_data[output.name].to_numpy().reshape((n_samples,))
+    y = clean_data[output.name].to_numpy().reshape((len(clean_data.index),))
 
     regression.fit(x, y)
 
     return regression.score(x, y)
 
+# returns p-value of chi-square test
 def get_uni_chi2_results(input: pd.Series, output: pd.Series):
     clean_data        = pd.concat([input, output], axis=1).dropna() # remove Nan values
     contingency_table = pd.crosstab(clean_data[output.name], clean_data[input.name])
@@ -34,9 +35,9 @@ def main() -> None:
                 log_logit += f"i: {var_i.name}  o: {var_o.name}  |  coef: {get_uni_logit_results(var_i, var_o)}\n"
                 log_chi2  += f"i: {var_i.name}  o: {var_o.name}  |  coef: {get_uni_chi2_results(var_i, var_o)}\n"
     
-    with open('results/uni_logit.txt', 'w') as f:
+    with open('results/uni_logit.txt', 'a') as f:
         f.write(log_logit)
-    with open('results/uni_chi2.txt', 'w') as f:
+    with open('results/uni_chi2.txt', 'a') as f:
         f.write(log_chi2)
 
 
--- variables.py
+++ variables.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-def get_binary_dist(column: pd.Series) -> tuple: 
+'''def get_binary_dist(column: pd.Series) -> tuple: 
     outcome_counts = column.value_counts().values # (2, 1)
 
     true_dist      = outcome_counts[1]/sum(outcome_counts)
     false_dist     = outcome_counts[0]/sum(outcome_counts)
 
-    return (true_dist, false_dist) 
+    return (true_dist, false_dist) '''
 
 def get_recoded_var(var_list: list, var_name: str) -> pd.Series:
     new_data = []
@@ -20,8 +20,8 @@ def get_recoded_var(var_list: list, var_name: str) -> pd.Series:
 
 
 # Import datasets
-df  = pd.read_csv('resources/XXHq.csv')
-dfN = pd.read_csv('resources/XXHqn.csv') # Contains calculated variables
+df  = pd.read_csv('resources/XXHq copy.csv')
+dfN = pd.read_csv('resources/XXHqn copy.csv') # Contains calculated variables
 
 # // SUFFIX MEANINGS //
 # _pt - probability table
@@ -30,11 +30,11 @@ dfN = pd.read_csv('resources/XXHqn.csv') # Contains calculated variables
 
 # // DIATHESIS //
 QN87    = dfN['qn87'] 
-QN87_pt = get_binary_dist(QN87) # Homelessness during pandemic
+#QN87_pt = get_binary_dist(QN87) # Homelessness during pandemic
 
 
 QN94    = dfN['qn94']
-QN94_pt = get_binary_dist(QN94) # Job loss
+#QN94_pt = get_binary_dist(QN94) # Job loss
 
 diathesis = [QN87, QN94]
 
@@ -49,7 +49,7 @@ Q23    = df['q23'] # Bullied at school
 Q24    = df['q24'] # Electronically Bullied
 
 Q23_new    = get_recoded_var([Q23,Q24], "q23_new") # Bullied (collective)
-Q23_new_pt = get_binary_dist(Q23_new)
+#Q23_new_pt = get_binary_dist(Q23_new)
 
 defeat = [QN22_new, Q23_new]
 
@@ -73,26 +73,26 @@ behavior = [QN28]
 # // MODERATORS // 
 # Threats to Self Moderators
 QN66     = dfN['qn66']
-QN66_pt  = get_binary_dist(QN66) # Negative Perception of Weight
+#QN66_pt  = get_binary_dist(QN66) # Negative Perception of Weight
 
 QN67        = dfN['qn67'] # Trying to lose weight
 QN67_new    = get_recoded_var([QN66, QN67], "qn67_new") 
-QN67_new_pt = get_binary_dist(QN67_new) # Negative Perception of Weight and Taking Action
+#QN67_new_pt = get_binary_dist(QN67_new) # Negative Perception of Weight and Taking Action
 
 Q98    = df['q98']
-Q98_pt = get_binary_dist(Q98) # Difficulty concentrating
+#Q98_pt = get_binary_dist(Q98) # Difficulty concentrating
 
 QNILLICT    = dfN['qnillict']
-QNILLICT_pt = get_binary_dist(QNILLICT) # Ever used selct illicit drugs
+#QNILLICT_pt = get_binary_dist(QNILLICT) # Ever used selct illicit drugs
 
 threats_to_self_moderators = [QN66, QN67_new, Q98, QNILLICT]
 
 # Motivational Moderators
 Q25    = df['q25']
-Q25_pt = get_binary_dist(Q25) # Sad or Hopeless
+#Q25_pt = get_binary_dist(Q25) # Sad or Hopeless
 
 QN96    = dfN['qn96']
-QN96_pt = get_binary_dist(QN96) # Feel close to people at school
+#QN96_pt = get_binary_dist(QN96) # Feel close to people at school
 
 motivational_moderators = [Q25, QN96]
 
@@ -101,18 +101,20 @@ QN12 = dfN['qn12'] # Carried weapon on school property
 QN13 = dfN['qn13'] # Carried a gun
 
 QN13_new    = get_recoded_var([QN12, QN13], "qn13_new") 
-QN13_new_pt = get_binary_dist(QN13_new) # Carried a weapon
+#QN13_new_pt = get_binary_dist(QN13_new) # Carried a weapon
 
 QN16    = dfN['qn16'] 
-QN16_pt = get_binary_dist(QN16) # Was in a physical fight
+#QN16_pt = get_binary_dist(QN16) # Was in a physical fight
 
 Q18    = df['q18']
-Q18_pt = get_binary_dist(Q18) # Saw physical violence in neighborhood
+#Q18_pt = get_binary_dist(Q18) # Saw physical violence in neighborhood
 
 Q27    = df['q27']
-Q27_pt = get_binary_dist(Q27) # Made a suicide plan
+#Q27_pt = get_binary_dist(Q27) # Made a suicide plan
 
 volitional_moderators = [QN13_new, QN16, Q18, Q27]
+
+all_var = pd.concat(diathesis+defeat+entrapment+ideation+behavior+threats_to_self_moderators+motivational_moderators+volitional_moderators, axis=1)
 
 # Relationship in the IMV Model
 relationships = (
