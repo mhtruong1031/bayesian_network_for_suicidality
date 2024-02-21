997bde345bb2a1ad555ce935f27c38fd8da657d1
mhtruong1031 <mhtruong1031@gmail.com>
2024-02-21 10:20:03 -0800

    cleanup

--- model.py
+++ model.py
@@ -29,27 +29,3 @@ for relation in relationships:
 model.add_nodes_from(['qn13_new', 'index', 'qn94'])
 model.add_edges_from(edges)
 model.fit(training_data)
-
-'''predict_data = {
-    'qn87': 1.0,
-    'qn94': 1.0,
-    'q23_new': 1.0,
-    'qn22_new': 1.0,
-    'q98': 2.0,
-    'qn93': 2.0,
-    'qn85': 2.0,
-    'q25': 2.0,
-    'qn66': 2.0,
-    'qn67_new': 2.0,
-    'qnillict': 1.0,
-    'qn96': 1.0,
-    'qn13_new': 1.0,
-    'qn16': 1.0,
-    'q18': 1.0,
-    'q27': 1.0
-}
-predict_data_df = pd.DataFrame(data=predict_data, index=[0])
-print(predict_data_df)
-print(model.predict_probability(predict_data_df)) # TO DO: only predicts properly in model.py? find out why
-
-BIFWriter(model).write_bif('model.bif')'''
